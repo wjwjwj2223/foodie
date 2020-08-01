6892c0837918663f8147648df5e0b58d06c63021
wangjie <17054214@qq.com>
2020-08-01 17:42:21 +0800

    swagger2  优化展示

--- foodie-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-api/src/main/java/com/imooc/controller/PassportController.java
@@ -3,10 +3,13 @@ package com.imooc.controller;
 import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
 import com.imooc.utils.IMOOCJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Api(value = "登录注册", tags = {"用于登录注册的相关接口"})
 @RestController
 @RequestMapping("passport")
 public class PassportController {
@@ -14,6 +17,7 @@ public class PassportController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "用户名是否存在", notes = "用户名是否存在", httpMethod = "GET")
     @GetMapping("/usernameIsExist")
     public IMOOCJSONResult usernameIsExist(@RequestParam String username) {
         //1. 判断用户名不能为空
@@ -29,6 +33,7 @@ public class PassportController {
         return IMOOCJSONResult.ok();
     }
 
+    @ApiOperation(value = "用户名注册", notes = "用户注册", httpMethod = "POST")
     @PostMapping("/regist")
     public IMOOCJSONResult regist(@RequestBody UserBO userBO) {
 
--- foodie-pojo/src/main/java/com/imooc/pojo/bo/UserBO.java
+++ foodie-pojo/src/main/java/com/imooc/pojo/bo/UserBO.java
@@ -1,9 +1,16 @@
 package com.imooc.pojo.bo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "用户对象BO", description = "从客户端，由用户传入的数据封装在此entity中")
 public class UserBO {
 
+    @ApiModelProperty(value = "用户名", name = "用户名", example = "imooc", required = true)
     private String username;
+    @ApiModelProperty(value = "密码", name = "密码", example = "123456", required = true)
     private String password;
+    @ApiModelProperty(value = "确认密码", name = "确认密码", example = "123456", required = true)
     private String confirmPassword;
 
     public String getUsername() {
