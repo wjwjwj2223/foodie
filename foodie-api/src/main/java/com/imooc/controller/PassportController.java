6346e78aa8c068a04ce123243ef1f8637593e8da
wangjie <17054214@qq.com>
2020-08-02 22:48:12 +0800

    退出登陆接口

--- foodie-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-api/src/main/java/com/imooc/controller/PassportController.java
@@ -94,6 +94,18 @@ public class PassportController {
         return IMOOCJSONResult.ok();
     }
 
+    @ApiOperation(value = "用户退出登录", notes = "用户退出登录", httpMethod = "POST")
+    @PostMapping("/logout")
+    public IMOOCJSONResult logout(@RequestParam String userId, HttpServletRequest request, HttpServletResponse response) {
+
+        //清除相关cookie
+        CookieUtils.deleteCookie(request, response, "user");
+        // TODO 用户退出登陆需要清空购物车
+        // TODO 分布式会话中需要清空用户数据
+        return IMOOCJSONResult.ok();
+    }
+
+
     private User setNullProperty(User user) {
         user.setPassword(null);
         user.setRealname(null);
