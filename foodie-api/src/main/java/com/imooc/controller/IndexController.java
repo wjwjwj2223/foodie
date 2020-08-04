5545484cc26c3b046d06926d3172c241d4759f93
wangjie <17054214@qq.com>
2020-08-04 22:53:05 +0800

    搜索商品列表  分页  销量价格排序等实现

--- foodie-api/src/main/java/com/imooc/controller/IndexController.java
+++ foodie-api/src/main/java/com/imooc/controller/IndexController.java
@@ -11,13 +11,11 @@ import com.imooc.utils.IMOOCJSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
