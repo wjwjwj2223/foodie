743f647446fa15c8d364c3e23545890259102f18
wangjie <17054214@qq.com>
2020-08-04 17:32:40 +0800

    查看商品评价等级数量

--- foodie-service/src/main/java/com/imooc/service/ItemService.java
+++ foodie-service/src/main/java/com/imooc/service/ItemService.java
@@ -4,6 +4,7 @@ import com.imooc.pojo.Items;
 import com.imooc.pojo.ItemsImg;
 import com.imooc.pojo.ItemsParam;
 import com.imooc.pojo.ItemsSpec;
+import com.imooc.pojo.vo.CommentLevelCountsVO;
 
 import java.util.List;
 
@@ -37,4 +38,9 @@ public interface ItemService {
      */
     public ItemsParam queryItemParamById(String itemId);
 
+    /**
+     * 根据商品id查询商品的评价等级数量
+     * @param itemId
+     */
+    public CommentLevelCountsVO queryCommentCounts(String itemId);
 }
