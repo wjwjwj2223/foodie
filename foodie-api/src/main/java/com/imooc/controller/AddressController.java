553d0f8bd00851561eab7a8bfccc9dd41174c7d5
wangjie <17054214@qq.com>
2020-08-05 19:01:24 +0800

    用户设置默认地址

--- foodie-api/src/main/java/com/imooc/controller/AddressController.java
+++ foodie-api/src/main/java/com/imooc/controller/AddressController.java
@@ -91,6 +91,20 @@ public class AddressController {
         return IMOOCJSONResult.ok();
     }
 
+    @ApiOperation(value = "用户设置默认地址", notes = "用户设置默认地址", httpMethod = "POST")
+    @PostMapping("/setDefalut")
+    public IMOOCJSONResult setDefalut(
+            @RequestParam String userId,
+            @RequestParam String addressId) {
+
+        if (StringUtils.isBlank(userId) || StringUtils.isBlank(addressId)) {
+            return IMOOCJSONResult.errorMsg("");
+        }
+
+        addressService.updateUserAddressToBeDefault(userId, addressId);
+        return IMOOCJSONResult.ok();
+    }
+
     private IMOOCJSONResult checkAddress(AddressBO addressBO) {
         String receiver = addressBO.getReceiver();
         if (StringUtils.isBlank(receiver)) {
