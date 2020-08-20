b64dec7434c56696f57ca8638e4dd78a6084058a
wangjie <17054214@qq.com>
2020-08-20 18:47:15 +0800

    修改用户信息接口

--- foodie-service/src/main/java/com/imooc/service/center/CenterUserService.java
+++ foodie-service/src/main/java/com/imooc/service/center/CenterUserService.java
@@ -1,6 +1,7 @@
 package com.imooc.service.center;
 
 import com.imooc.pojo.User;
+import com.imooc.pojo.bo.center.CenterUserBO;
 
 public interface CenterUserService {
 
@@ -11,4 +12,10 @@ public interface CenterUserService {
      */
     public User queryUserInfo(String userId);
 
+    /**
+     * 修改用户信息
+     * @param userId
+     * @param centerUserBO
+     */
+    public User updateUserInfo(String userId, CenterUserBO centerUserBO);
 }
--- foodie-service/src/main/java/com/imooc/service/impl/center/CenterUserServiceImpl.java
+++ foodie-service/src/main/java/com/imooc/service/impl/center/CenterUserServiceImpl.java
@@ -2,12 +2,16 @@ package com.imooc.service.impl.center;
 
 import com.imooc.mapper.UserMapper;
 import com.imooc.pojo.User;
+import com.imooc.pojo.bo.center.CenterUserBO;
 import com.imooc.service.center.CenterUserService;
 import org.n3r.idworker.Sid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
 public class CenterUserServiceImpl implements CenterUserService {
@@ -24,6 +28,17 @@ public class CenterUserServiceImpl implements CenterUserService {
         User user = usersMapper.selectByPrimaryKey(userId);
         user.setPassword(null);
         return user;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public User updateUserInfo(String userId, CenterUserBO centerUserBO) {
+        User updateUser = new User();
+        BeanUtils.copyProperties(centerUserBO, updateUser);
+        updateUser.setId(userId);
+        updateUser.setUpdatedTime(new Date());
+        usersMapper.updateByPrimaryKeySelective(updateUser);
+        return queryUserInfo(userId);
     }
 
 }
