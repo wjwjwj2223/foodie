5f34508c55acae1cd7cef4fd1607d2de16a98cb3
wangjie <17054214@qq.com>
2020-09-08 23:42:09 +0800

    redis批量查询

--- foodie-api/src/main/java/com/imooc/controller/RedisController.java
+++ foodie-api/src/main/java/com/imooc/controller/RedisController.java
@@ -1,10 +1,15 @@
 package com.imooc.controller;
+import com.imooc.utils.RedisOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @ApiIgnore
@@ -13,6 +18,9 @@ public class RedisController {
 
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private RedisOperator redisOperator;
 
     @GetMapping("/set")
     public Object set(String key, String value) {
@@ -31,5 +39,29 @@ public class RedisController {
         return "delete ok";
     }
 
+    /**
+     * 大量key查询
+     * @param keys
+     * @return
+     */
+    @GetMapping("/getALot")
+    public Object getALot(String... keys) {
+        List<String> resutl = new ArrayList<>();
+        for (String k:keys) {
+            resutl.add(redisOperator.get(k));
+        }
+        return resutl;
+    }
+
+    /**
+     * 批量查询 mget
+     * @param keys
+     * @return
+     */
+    @GetMapping("/mget")
+    public Object mget(String... keys) {
+        List<String> keysList = Arrays.asList(keys);
+        return redisOperator.mget(keysList);
+    }
 
 }
