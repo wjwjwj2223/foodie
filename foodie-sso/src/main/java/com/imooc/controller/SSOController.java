f5e3196002eb6cbf3186e3ea323ec33302a61f56
wangjie <17054214@qq.com>
2020-09-22 19:18:31 +0800

    ssl 项目搭建

---
+++ foodie-sso/src/main/java/com/imooc/controller/SSOController.java
@@ -0,0 +1,19 @@
+package com.imooc.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+@Controller
+public class SSOController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public Object hello() {
+        return "Hello world";
+    }
+}
